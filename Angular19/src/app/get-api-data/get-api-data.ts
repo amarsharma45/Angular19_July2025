df248a7047dc18264168c6ceb2c0312d5902e980
amarsharma45 <52012163+amarsharma45@users.noreply.github.com>
2025-07-28 22:22:37 +0530

    apicalls

--- Angular19/src/app/get-api-data/get-api-data.ts
+++ Angular19/src/app/get-api-data/get-api-data.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -10,11 +10,54 @@ import { Component } from '@angular/core';
 export class GetApiData {
     //https://json-placeholder.mock.beeceptor.com/roles
     userList:any[]=[];
+
+    pagedUsers: any[] = [];
+
+    pageSize: number = 5;
+    currentPage: number = 1;
+    totalPages: number = 0;
+
+
     constructor(private http:HttpClient) {}
+  
     getusers()
     {
-      this.http.get("https://amarsharma-cafse9c4dzgbf2hc.eastus2-01.azurewebsites.net/api/product").subscribe((res:any)=>{
-        this.userList=res;
+        const token = localStorage.getItem('token');
+
+        const headers = new HttpHeaders({
+          Authorization: `Bearer ${token}`
+        });
+
+     this.http.get<any[]>("https://amarsharma-cafse9c4dzgbf2hc.eastus2-01.azurewebsites.net/api/employees", { headers })
+      .subscribe({
+        next: (res: any[]) => {
+          if (Array.isArray(res)) {
+            this.userList = res;
+            this.currentPage = 1;
+            this.updatePagedUsers();
+          } else {
+            console.error("API did not return an array:", res);
+          }
+        },
+        error: (err) => {
+          console.error("API error:", err);
+        }
       });
+
+    }
+    updatePagedUsers() {
+    const totalItems = this.userList?.length ?? 0;
+    this.totalPages = Math.ceil(totalItems / this.pageSize);
+
+    const start = (this.currentPage - 1) * this.pageSize;
+    const end = start + this.pageSize;
+
+    this.pagedUsers = this.userList.slice(start, end);
+    }
+
+    goToPage(page: number) {
+      if (page < 1 || page > this.totalPages) return;
+      this.currentPage = page;
+      this.updatePagedUsers();
     }
 }
--- Angular19/src/app/login/login.ts
+++ Angular19/src/app/login/login.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -8,11 +9,32 @@ import { Component } from '@angular/core';
   standalone: true,
 })
 export class Login {
-
+  constructor(private http: HttpClient) {}
 CombinedName: string = '';
-
+  userId: number = 1;
+  name: string = 'admin';
+  password: string = 'password123';
+  token: string = '';
   login(userId:number, Name:string){
-    alert(' -- Login button clicked!');
-    this.CombinedName =  'Logged in with' + userId+' '+Name;
+    // alert(' -- Login button clicked!');
+    // this.CombinedName =  'Logged in with' + userId+' '+Name;
+  
+    const loginData = {
+      username: this.name,
+      password: this.password
+    };
+
+      this.http.post<any>('https://amarsharma-cafse9c4dzgbf2hc.eastus2-01.azurewebsites.net/api/auth/login', loginData)
+        .subscribe({
+          next: (response) => {
+            this.token = response.token;
+            localStorage.setItem('token', this.token);
+            alert('Login successful!');
+          },
+          error: (err) => {
+            alert('Login failed: ' + err.error);
+          }
+        });
+    
   }
 }
