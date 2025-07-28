c422330b424a7416b336504f3000adaa16de8444
amarsharma45 <52012163+amarsharma45@users.noreply.github.com>
2025-07-28 13:36:31 +0530

    added my api

--- Angular19/src/app/get-api-data/get-api-data.ts
+++ Angular19/src/app/get-api-data/get-api-data.ts
@@ -13,7 +13,7 @@ export class GetApiData {
     constructor(private http:HttpClient) {}
     getusers()
     {
-      this.http.get("https://json-placeholder.mock.beeceptor.com/roles").subscribe((res:any)=>{
+      this.http.get("https://amarsharma-cafse9c4dzgbf2hc.eastus2-01.azurewebsites.net/api/product").subscribe((res:any)=>{
         this.userList=res;
       });
     }
